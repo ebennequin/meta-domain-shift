d8dd3690beb8cc50fb492af6fdaf4044fbf23664
etienne <etienne.bennequin@gmail.com>
2021-01-07 11:52:48 +0100

    Add logging values of different loss terms in OT for tensorboard

--- src/methods/optimal_transport.py
+++ src/methods/optimal_transport.py
@@ -1,5 +1,8 @@
 import torch
 import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+
+from configs import experiment_config
 from src.methods.abstract_meta_learner import AbstractMetaLearner
 from src.methods.sinkhorn import Sinkhorn
 from src.methods.utils import one_hot
@@ -15,6 +18,8 @@ class OptimalTransport(AbstractMetaLearner):
         self.sinkhorn = Sinkhorn(
             eps=regularization, max_iter=max_iter, thresh=stopping_criterion
         )
+        self.episode_count = 0
+        self.loss_writer = SummaryWriter(log_dir=experiment_config.SAVE_DIR)
 
     def set_forward(self, support_images, support_labels, query_images):
         """
@@ -48,6 +53,16 @@ class OptimalTransport(AbstractMetaLearner):
 
         scores = torch.log(probs)
         classification_loss = self.loss_fn(scores, query_labels)
+
+        if self.training:
+            self.loss_writer.add_scalar(
+                "Train/classification_loss", classification_loss, self.episode_count
+            )
+            self.loss_writer.add_scalar(
+                "Train/transport_cost", cost, self.episode_count
+            )
+            self.episode_count = self.episode_count + 1
+
         loss = classification_loss + self.lambda_cost * cost
 
         return scores, loss
