4567f7adf8e35381332f516243073421090d7891
hawat <etienne.bennequin@gmail.com>
2021-02-19 13:36:25 +0100

    Add extra evaluation with forced transportation

--- configs/all_datasets_configs/cifar_100_c_config.py
+++ configs/all_datasets_configs/cifar_100_c_config.py
@@ -4,6 +4,6 @@ from src.datasets import CIFAR100CMeta
 
 DATASET = CIFAR100CMeta
 IMAGE_SIZE = 32
-DATA_ROOT = Path("./data")
+DATA_ROOT = Path("/media/etienneb")
 SPECS_ROOT = Path("configs/dataset_specs/cifar_100_c")
 CLASSES = {"train": 65, "val": 10, "test": 25}
--- configs/experiment_config.py
+++ configs/experiment_config.py
@@ -5,4 +5,8 @@ from pathlib import Path
 RANDOM_SEED = 1  # If None, random seed will be randomly sampled
 SAVE_RESULTS = True
 OVERWRITE = False  # If True, will erase all previous content of SAVE_DIR
-SAVE_DIR = Path("output") / "results"
+SAVE_DIR = Path("output")
+
+# Additional parameters, only used for polyaxon
+USE_POLYAXON = True
+SECOND_EVAL_SAVE_DIR = SAVE_DIR / "extra_eval"
