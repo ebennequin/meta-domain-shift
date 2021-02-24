188829569815ca91371138e56a0c0348b59dd372
etienne <etienne.bennequin@gmail.com>
2021-02-24 11:15:38 +0100

    Add final accuracy log in tensorboard

--- scripts/run_experiment.py
+++ scripts/run_experiment.py
@@ -1,5 +1,8 @@
+"""
+Run a complete experiment (training + evaluation)
+"""
+
 import torch
-from polyaxon_client.tracking import Experiment
 
 from src.running_steps import (
     train_model,
@@ -8,13 +11,7 @@ from src.running_steps import (
     prepare_output,
 )
 
-"""
-Run a complete experiment (training + evaluation)
-"""
-
 if __name__ == "__main__":
-    # Warning: this fails if outside a polyaxon managed container
-    experiment = Experiment()
 
     prepare_output()
 
@@ -24,5 +21,3 @@ if __name__ == "__main__":
 
     set_and_print_random_seed()
     acc = eval_model(trained_model)
-
-    experiment.log_metrics(accuracy=acc)
